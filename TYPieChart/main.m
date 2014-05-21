53cceed2f3272150503c6884356f75d11bda366e
田雨 <ty110@tianyudeiMac.local>
2014-05-21 15:32:07 +0800

    Initial Commit

---
+++ TYPieChart/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TYPieChart
+//
+//  Created by 田雨 on 14-5-21.
+//  Copyright (c) 2014年 tianyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ TYPieChartTests/TYPieChartTests.m
@@ -0,0 +1,34 @@
+//
+//  TYPieChartTests.m
+//  TYPieChartTests
+//
+//  Created by 田雨 on 14-5-21.
+//  Copyright (c) 2014年 tianyu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TYPieChartTests : XCTestCase
+
+@end
+
+@implementation TYPieChartTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
